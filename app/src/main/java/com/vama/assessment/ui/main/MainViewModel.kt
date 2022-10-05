252c35d604dc35193b2ab0a1c6023b7c9955e7e2
mnajjja <84813278+mnajjja@users.noreply.github.com>
2022-10-05 22:49:17 +0300

    Update MainViewModel.kt

--- app/src/main/java/com/vama/assessment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/vama/assessment/ui/main/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.vama.assessment.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
